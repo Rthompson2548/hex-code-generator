625581f21f5415f14787fbd7b8ae7370d6d580a4
reillythompson <80596387+Rthompson2548@users.noreply.github.com>
2022-03-06 17:21:01 -0800

    Revert "reverting"
    This reverts commit 43771f76392c292b50caa8e32e03466d23021062.

--- src/App.js
+++ src/App.js
@@ -18,21 +18,32 @@ function App() {
   const defaultColors = [
     {
       name: "Red",
+<<<<<<< HEAD
       buttonColor: "#DC143C",
+=======
+>>>>>>> parent of 37d673d (updated color values)
       code: "#FF0000",
     },
     {
       name: "Orange",
+<<<<<<< HEAD
       buttonColor: "#FF8C00",
       code: "#FFA500",
     },
     {
       name: "Yellow",
       buttonColor: "#F7DA63",
+=======
+      code: "#ffa500",
+    },
+    {
+      name: "Yellow",
+>>>>>>> parent of 37d673d (updated color values)
       code: "#FFFF00",
     },
     {
       name: "Green",
+<<<<<<< HEAD
       buttonColor: "#3CB371",
       code: "#00ff00",
     },
@@ -45,6 +56,17 @@ function App() {
       name: "Purple",
       buttonColor: "#DDA0DD",
       code: "#6a0dad",
+=======
+      code: "#008000",
+    },
+    {
+      name: "Blue",
+      code: "#0000FF",
+    },
+    {
+      name: "Purple",
+      code: "#800080",
+>>>>>>> parent of 37d673d (updated color values)
     },
     {
       name: "Pink",
@@ -56,10 +78,17 @@ function App() {
       buttonColor: "#000000",
       code: "#000000",
     },
+<<<<<<< HEAD
     // {
     //   name: "White",
     //   buttonColor: "#D3D3D3",
     // },
+=======
+    {
+      name: "White",
+      code: "#ffffff",
+    },
+>>>>>>> parent of 37d673d (updated color values)
   ];
 
 
