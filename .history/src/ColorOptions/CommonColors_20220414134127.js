31e58ea9b8a0ed389828ba365a29709096bfee8f
reillythompson <80596387+Rthompson2548@users.noreply.github.com>
2022-04-14 14:30:18 -0700

    updated css

---
+++ .history/src/ColorOptions/CommonColors_20220414134127.js
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from "react";
+
+const CommonColors = ({ defaultColors, handleSubmitDefaultColor, hexCode }) => {
+  const hexCodeValue = `#${hexCode}`;
+
+    return (
+        <ul className="color-buttons-container">            
+              {defaultColors.map((color, index) => {
+                return <li key={index}
+                          className="color-buttons-li">
+                            <button
+                                      /** sets value to the hex code for the color */
+                                      value={color.code}
+                                      style={{ backgroundColor: color.code }}
+                                      className="color-button"
+                                      onClick={handleSubmitDefaultColor}>
+                                      
+                                <div className='color-button-text'>
+                                    <p>{color.name}</p>
+                                    <p>{color.year}</p>
+                                    <p>{color.code}</p>  
+                              </div>                   
+                  </button>
+                  
+                      </li>                      
+                    })}
+          </ul>
+    )
+}
+
+export default CommonColors;
---
+++ .history/src/ColorOptions/CommonColors_20220414141551.js
@@ -0,0 +1,29 @@
+import React, {useEffect, useState} from "react";
+
+const CommonColors = ({ defaultColors, handleSubmitDefaultColor, hexCode }) => {
+  const hexCodeValue = `#${hexCode}`;
+
+    return (
+        <ul className="color-buttons-container">            
+              {defaultColors.map((color, index) => {
+                return <li key={index}>
+                            <button
+                                      /** sets value to the hex code for the color */
+                                      value={color.code}
+                                      style={{ backgroundColor: color.code }}
+                                      className="color-button"
+                                      onClick={handleSubmitDefaultColor}>
+                                      
+                                <div className='color-button-text'>
+                      <p>{color.name}</p>
+                      <p>{color.code}</p>                                    
+                              </div>                   
+                  </button>
+                  
+                      </li>                      
+                    })}
+          </ul>
+    )
+}
+
+export default CommonColors;
