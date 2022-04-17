29a35d52775ee325a008f8eaa3fda292db2e1a23
reillythompson <80596387+Rthompson2548@users.noreply.github.com>
2022-04-17 10:05:17 -0700

    added error message for invalid input field

--- src/HexGenerator/GenerateColors/GenerateColors.js
+++ src/HexGenerator/GenerateColors/GenerateColors.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Values from "values.js";
 import "../../App.scss";
 import "./GenerateColors.scss"
@@ -6,9 +6,14 @@ import "./GenerateColors.scss"
 
 const GenerateColors = ({ hexCode, setHexCode, setHexCodeList }) => {
 
+    const [inputValueError, setInputValueError] = useState(false);
+
     const generateAnalogousColors = (event) => {
+        if (hexCode.length < 1) {
+            setInputValueError(true);
+        }
+
     event.preventDefault();
-   
       let hexCodes = new Values(hexCode).all(10);
       setHexCodeList(hexCodes);
       setHexCode("");
@@ -17,9 +22,19 @@ const GenerateColors = ({ hexCode, setHexCode, setHexCodeList }) => {
 
     return (
         <div className="generate-colors">                      
-            <button onClick={generateAnalogousColors} type="submit">
-                  Generate colors 
-            </button>       
+            <button
+                onClick={generateAnalogousColors}
+                type="submit">
+                Generate colors 
+            </button>
+            
+            {inputValueError === true && (
+                <div>
+                    <h1>Error!!!!</h1>
+                </div>
+            )
+                
+            }
         </div>
     )
 }
