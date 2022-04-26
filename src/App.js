09b95006b02131693ae4100f54878b9da2837d29
reillythompson <80596387+Rthompson2548@users.noreply.github.com>
2022-04-26 14:42:04 -0700

    removed unused state variables from app component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,10 @@ import HexGenerator from './HexGenerator';
 import "./App.scss";
 
 function App() {
-  /** states and event handlers for setting hex code(s) and displaying colors */
-  const [showColors, setShowColors] = useState(false); 
-
+ 
   return (
     <div className='App-container'>
       <HexGenerator
-        showColors={showColors}
-        setShowColors={setShowColors}
       />
     </div>
   )
